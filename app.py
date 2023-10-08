7295c86b78bb954daf572232c052d62018f1a1ba
NGV <95875573+ngv-sama@users.noreply.github.com>
2023-10-08 14:41:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ Answer: Let's think step by step."""
 prompt = PromptTemplate(template=template, input_variables=["question"])
 
 llm_chain = LLMChain(prompt=prompt,
-                     llm=HuggingFaceHub(repo_id="google/flan-t5-xxl",
+                     llm=HuggingFaceHub(repo_id="deepset/roberta-base-squad2",
                                         model_kwargs={"temperature": 0,
                                                       "max_length": 64}))
 
