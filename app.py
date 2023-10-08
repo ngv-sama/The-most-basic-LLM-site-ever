5648b85fa7c2ad6bf6fcd2d27d7539aa2fce1325
NGV <95875573+ngv-sama@users.noreply.github.com>
2023-10-08 14:38:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ Answer: Let's think step by step."""
 prompt = PromptTemplate(template=template, input_variables=["question"])
 
 llm_chain = LLMChain(prompt=prompt,
-                     llm=HuggingFaceHub(repo_id="google/flan-t5-large",
+                     llm=HuggingFaceHub(repo_id="google/flan-t5-xxl",
                                         model_kwargs={"temperature": 0,
                                                       "max_length": 64}))
 
